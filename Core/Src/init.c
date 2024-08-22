fa65c5b386b2fba88cb8ae7d81cbf6851bcb1455
ocanath <ocanath@gmail.com>
2024-08-22 11:55:43 -0700

    changed fdcan

--- Core/Src/init.c
+++ Core/Src/init.c
@@ -80,20 +80,20 @@ void MX_FDCAN1_Init(void)
 	/* USER CODE END FDCAN1_Init 1 */
 	hfdcan1.Instance = FDCAN1;
 	hfdcan1.Init.ClockDivider = FDCAN_CLOCK_DIV1;
-	hfdcan1.Init.FrameFormat = FDCAN_FRAME_CLASSIC;
+	hfdcan1.Init.FrameFormat = FDCAN_FRAME_FD_BRS;
 	hfdcan1.Init.Mode = FDCAN_MODE_NORMAL;
 	hfdcan1.Init.AutoRetransmission = DISABLE;
 	hfdcan1.Init.TransmitPause = DISABLE;
 	hfdcan1.Init.ProtocolException = DISABLE;
-	hfdcan1.Init.NominalPrescaler = 16;
-	hfdcan1.Init.NominalSyncJumpWidth = 1;
-	hfdcan1.Init.NominalTimeSeg1 = 2;
-	hfdcan1.Init.NominalTimeSeg2 = 2;
+	hfdcan1.Init.NominalPrescaler = 1;
+	hfdcan1.Init.NominalSyncJumpWidth = 16;
+	hfdcan1.Init.NominalTimeSeg1 = 63;
+	hfdcan1.Init.NominalTimeSeg2 = 16;
 	hfdcan1.Init.DataPrescaler = 1;
-	hfdcan1.Init.DataSyncJumpWidth = 1;
-	hfdcan1.Init.DataTimeSeg1 = 1;
-	hfdcan1.Init.DataTimeSeg2 = 1;
-	hfdcan1.Init.StdFiltersNbr = 0;
+	hfdcan1.Init.DataSyncJumpWidth = 4;
+	hfdcan1.Init.DataTimeSeg1 = 5;
+	hfdcan1.Init.DataTimeSeg2 = 4;
+	hfdcan1.Init.StdFiltersNbr = 1;
 	hfdcan1.Init.ExtFiltersNbr = 0;
 	hfdcan1.Init.TxFifoQueueMode = FDCAN_TX_FIFO_OPERATION;
 	if (HAL_FDCAN_Init(&hfdcan1) != HAL_OK)
@@ -103,6 +103,7 @@ void MX_FDCAN1_Init(void)
 	/* USER CODE BEGIN FDCAN1_Init 2 */
 
 	/* USER CODE END FDCAN1_Init 2 */
+
 
 }
 
