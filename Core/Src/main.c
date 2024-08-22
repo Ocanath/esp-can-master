fae1629be35284c9e1d5cd74a0b3004bcabd94c4
ocanath <ocanath@gmail.com>
2024-08-21 20:12:37 -0700

    upgraded message structure to support message types, with a length parameter to support even payload lengths, 16 bit checksum, and padding to ensure message length is always consistent. this is all onthe python client side.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,24 +18,35 @@ void m_uart2_rx_cplt_callback(uart_it_t * h)
 
 }
 
-static int16_t can_tx_id = 0;
-static uint8_t can_tx_buf[8] = {0};
+
+typedef struct uart_can_request_t
+{
+	uint8_t type;
+	uint8_t len;
+	int16_t can_tx_id;
+	uint8_t can_tx_buf[8];
+
+}uart_can_request_t;
+uart_can_request_t gl_crq = {0};
+
 static uint8_t trigger_can_tx = 0;
 
 void ppp_rx_cplt_callback(uart_it_t * h)
 {
-	if(h->ppp_unstuffed_size == 12)	//2 address, 8 payload, 2 checksum
+	if(h->ppp_unstuffed_size != 0 && (h->ppp_unstuffed_size % 2) == 0)	//nonzero and even is our basic callback entry filter
 	{
 		const uint8_t * pbu8 = (uint8_t*)(&m_huart2.ppp_unstuff_buf[0]);	//alias for this so it's easier to type
 		const uint16_t * pbu16 = (uint16_t*)(&m_huart2.ppp_unstuff_buf[0]);	//alias for this so it's easier to type
-		int i16_size = h->ppp_unstuffed_size / sizeof(int16_t);
-		uint16_t checksum = fletchers_checksum16((uint16_t*)pbu16, i16_size - 1);
-		if(checksum == pbu16[i16_size-1])
+		int i16_size = h->ppp_unstuffed_size / sizeof(int16_t);	//even, so this is fine
+		uint16_t checksum = fletchers_checksum16((uint16_t*)pbu16, i16_size - 1);	//checksum is always the last two bytes
+		if(checksum == pbu16[i16_size-1])		//compare calculated against received
 		{
-			can_tx_id = pbu16[0];
-			for(int i = 0; i < sizeof(can_tx_buf); i++)
+			gl_crq.type = pbu8[0];
+			gl_crq.len = pbu8[1];
+			gl_crq.can_tx_id = pbu16[1];
+			for(int i = 0; (i < gl_crq.len) && (i < sizeof(gl_crq.can_tx_buf)); i++)
 			{
-				can_tx_buf[i] = pbu8[i+sizeof(can_tx_id)];
+				gl_crq.can_tx_buf[i] = pbu8[i+4];	//start at 4, go until you hit either the requested length or run out of buffer to write to
 			}
 			trigger_can_tx = 1;
 		}
@@ -64,7 +75,7 @@ int main(void)
 			led_ts = tick;	//led stays on for 10ms if there is can tx activity (or rx activity?)
 			HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, 1);
 
-			int len = PPP_stuff(can_tx_buf, sizeof(can_tx_buf), gl_ppp_stuff_buf, sizeof(gl_ppp_stuff_buf));
+			int len = PPP_stuff(gl_crq.can_tx_buf, gl_crq.len, gl_ppp_stuff_buf, sizeof(gl_ppp_stuff_buf));
 			m_uart_tx_start(&m_huart2, gl_ppp_stuff_buf, len);
 			trigger_can_tx = 0;
 		}
--- Python/chat.py
+++ Python/chat.py
@@ -3,15 +3,24 @@ import time
 from checksum import *
 from PPP_stuffing import *
 
-
-def create_can_payload(id, can_payload):
-
-	id_bytes = struct.pack('<H', id)
-	payload = id_bytes + can_payload
+"""
+This payload structure:
+[msg type][actual can tx length][CAN message ID, 2 bytes][can payload, even number of bytes][16 bit fletcher's checksum]
+"""
+def create_can_payload(msg_type, tx_len, id, can_payload):
+	payload = struct.pack('<b', msg_type)
+	payload = payload + struct.pack('<b', tx_len)
+	payload = payload + struct.pack('<H', id)
+	if(len(can_payload) % 2 != 0):
+		can_payload = can_payload + bytes(1)	#zero pad the payload so that we can calculate a fletcher's checksum. Worst case time is added 1 byte, but mostly we're sending 4 bytes anyways so it's a fine choice
+	payload = payload + can_payload
 	chk = fletchers_checksum16(payload)
 	chk_bytes = struct.pack('<H', chk)
 	payload = payload + chk_bytes
 	return payload
+
+
+
 
 if __name__ == "__main__":
 
@@ -36,23 +45,22 @@ if __name__ == "__main__":
 		if(cur_time - ts > 0.5):
 			ts = cur_time
 
-			can_payload = bytearray(8)
+			can_payload = bytearray(5)
 			can_payload[0] = ord('h')
 			can_payload[1] = ord('e')
 			can_payload[2] = ord('l')
 			can_payload[3] = ord('l')
 			can_payload[4] = ord('o')
-			can_payload[5] = ord('?')
 
-			payload = create_can_payload(1, can_payload)
+			payload = create_can_payload(0, len(can_payload), 1, can_payload)
 			pld_stuffed = PPP_stuff(payload)
-			print(pld_stuffed)
+			# print(pld_stuffed)
 
 			server_socket.sendto(pld_stuffed, target_addr)
 
 		try:
 			pkt,source_addr = server_socket.recvfrom(512)
-			print("From: "+source_addr[0]+":"+str(source_addr[1])+": "+str(pkt))
+			print("From: "+source_addr[0]+":"+str(source_addr[1])+": ["+str(pkt) + "],  " + str(len(pkt)) + " bytes" )
 		except BlockingIOError:
 			pass
 
