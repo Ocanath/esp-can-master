432ccb783cca2f9fa4e76f37b4e61ca15ab7c24f
ocanath <ocanath@gmail.com>
2025-06-02 19:26:47 -0700

    barebones

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -42,28 +42,7 @@ void ppp_rx_cplt_callback(uart_it_t * h)
 {
 	if(h->ppp_unstuffed_size != 0 && (h->ppp_unstuffed_size % 2) == 0)	//nonzero and even is our basic callback entry filter
 	{
-		const uint8_t * pbu8 = (uint8_t*)(&m_huart2.ppp_unstuff_buf[0]);	//alias for this so it's easier to type
-		const uint16_t * pbu16 = (uint16_t*)(&m_huart2.ppp_unstuff_buf[0]);	//alias for this so it's easier to type
-		const int32_t * pbi32 = (int32_t * )(&m_huart2.ppp_unstuff_buf[2]);	//alias for section of payload corresponding to 32bit target values
-		int i16_size = h->ppp_unstuffed_size / sizeof(int16_t);	//must always be even, so this is fine
-		uint16_t checksum = fletchers_checksum16((uint16_t*)pbu16, i16_size - 1);	//checksum is always the last two bytes
-		if(checksum == pbu16[i16_size-1])		//compare calculated against received
-		{
-			/*
-			 * 0: mode
-			 * 1: pad
-			 * 2-3-4-5: w1
-			 * 6-7-8-9: w2
-			 * 10-11-12-13: w3
-			 * 14-15: chk
-			 */
-			gl_crq.mode = pbu8[0];
-			for(int i = 0; i < NUM_MOTORS && i*sizeof(int32_t) < sizeof(m_huart2.ppp_unstuff_buf); i++)
-			{
-				gl_crq.commands[i] = pbi32[i];
-			}
-			uart_buf_received = 1;
-		}
+		uart_buf_received = 1;
 	}
 }
 
@@ -165,47 +144,10 @@ int main(void)
 	FDCAN_Config();
 
 	uint32_t led_ts = 0;
-	uint32_t can_tx_ts = 0;
 
-	HAL_Delay(1000);
-
-	int tx_ididx = 0;
-	int32_t m0_offset = -990;
-	int32_t m1_offset = 16921;
-	send_motor_i32(motors[0].id, m0_offset);
-	HAL_Delay(1);
-	send_motor_i32(motors[1].id, m1_offset);
-	HAL_Delay(1);
-
-	for(int i = 0; i < NUM_MOTORS; i++)
-	{
-		send_misc_i32(motors[i].id, CHANGE_PCTL_VQ_OUTSAT, 3546);
-		HAL_Delay(1);
-//		send_misc_u8(motors[i].id, SET_PCTL_VQ_MODE, 0);	//load offsetted target (0) , then enable pctl_vq. also potentially change pctl gains
-	}
-
-//	send_misc_i32(3, CHANGE_PCTL_VQ_KP_VALUE, 30);
-//	send_misc_i32(3, CHANGE_PCTL_VQ_KP_RADIX, 5);
-//	send_misc_i32(3, CHANGE_PCTL_VQ_KI_VALUE, 1);
-//	send_misc_i32(3, CHANGE_PCTL_VQ_KI_RADIX, 10);
-//	send_misc_i32(3, CHANGE_PCTL_VQ_OUTSAT, 3000);
-//
-//	send_misc_i32(motors[0].id, CHANGE_PCTL_VQ_OUTSAT, 3546);
-//	send_misc_i32(2, CHANGE_PCTL_VQ_OUTSAT, 3546);
-
-	uint8_t trigger_can_tx = 0;
-
-	iirSOS upsampling_filter[NUM_MOTORS] = {0};
-	for(int i = 0; i < NUM_MOTORS; i++)
-	{
-		m_mcpy(&upsampling_filter[i], &gl_upsampling_filter, sizeof(iirSOS));
-	}
 	while (1)
 	{
 		uint32_t tick = HAL_GetTick();
-
-		motors[0].can_command = wrap_2pi_14b(gl_crq.commands[0] + m0_offset);	//todo: verify sign is correct
-		motors[1].can_command = wrap_2pi_14b(gl_crq.commands[1] + m1_offset);
 
 		/*Handle comms*/
 		if(uart_buf_received != 0)
@@ -232,35 +174,6 @@ int main(void)
 			len = PPP_stuff(firststuff, len, gl_ppp_stuff_buf, sizeof(gl_ppp_stuff_buf));	//double stuff the buffer! AAAH
 			m_uart_tx_start(&m_huart2, gl_ppp_stuff_buf, len);
 		}
-
-
-
-
-
-
-		/*in-loop receive and transmit*/
-		if((tick - can_tx_ts) > 10 || trigger_can_tx != 0)	//TODO: increase bandwidth by adding trigger for TX when we get a can RX
-		{
-			trigger_can_tx = 0;
-
-			can_tx_ts = tick;
-			//testval = sin_12b(wrap_2pi_12b(tick*10 + (PI_12B*i/3)))*4;
-			send_motor_i32(motors[tx_ididx].id, motors[tx_ididx].can_command);
-			tx_ididx = (tx_ididx + 1) % NUM_MOTORS;
-		}
-
-		if(HAL_FDCAN_GetRxFifoFillLevel(&hfdcan1, FDCAN_RX_FIFO0) != 0)
-		{
-			HAL_FDCAN_GetRxMessage(&hfdcan1, FDCAN_RX_FIFO0, &can_rx_header, can_rx_data.d);
-			{
-				trigger_can_tx = 1;	//if we received a reply before our pending timeout, trigger another tx  before the 10ms timeout!
-				uint16_t id = can_rx_header.Identifier;//note, test this, should retrieve correct ID
-				motors[id-motors[0].id].position = can_rx_data.i32[0];	//
-				motors[id-motors[0].id].current = can_rx_data.i16[2];
-				motors[id-motors[0].id].velocity = can_rx_data.i16[3];
-			}
-		}
-
 
 		/*LED blink*/
 		if(tick - led_ts > 100)
