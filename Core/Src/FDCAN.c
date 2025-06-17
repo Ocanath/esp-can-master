179b6ca9703f0d4b07bdbab9fbd5365815c9a4ed
ocanath <ocanath@gmail.com>
2025-06-17 11:29:19 -0700

    moved fdcan functions into fdcan. they're not complex enough to warrant unit testing, and the length management checking is now in the library handler

--- Core/Src/FDCAN.c
+++ Core/Src/FDCAN.c
@@ -18,6 +18,7 @@
  *
  */
 #include <FDCAN.h>
+#include "serial-comms-FDCAN.h"
 
 #define DEFAULT_V_REPORT_RADIX	4
 #define DEFAULT_IQ_RSHIFT		3	//estimated max value of iq is in the ~17bit domain. shift 3 to get it between 14-15bit
@@ -73,4 +74,29 @@ void FDCAN_Config(void)
 }
 
 
+/*Wrapper function for sending a can frame*/
+int send_can_frame(can_frame_t * frame)
+{
+	can_tx_header.Identifier = frame->id;
+	can_tx_header.DataLength = (frame->length & 0xFF) << 16;	//shift the length up for the interface
+	return HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &can_tx_header, frame->data);
+}
 
+
+/*Wrapper function for loading can frames into a can frame structure.
+ *
+ * This may serve as a template function if performance-optimized code is desired in the main loop.
+ * */
+int get_can_frame(can_frame_t * frame)
+{
+	if(HAL_FDCAN_GetRxFifoFillLevel(&hfdcan1, FDCAN_RX_FIFO0) != 0)
+	{
+		HAL_FDCAN_GetRxMessage(&hfdcan1, FDCAN_RX_FIFO0, &can_rx_header, frame->data);
+		{
+			frame->id = can_rx_header.Identifier;
+			frame->length = can_rx_header.DataLength;
+			return 0;
+		}
+	}
+	return 1;
+}
